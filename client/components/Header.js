c36a05f0b8b2d06aa61832e6c256fdabe9e5e819
IgorTimo <rostovplace@yandex.ru>
2022-04-23 15:04:12 +0300

    added log in button

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,26 @@
-import { Menu } from "semantic-ui-react";
-import Link from "next/link"
+import { Button, Menu } from "semantic-ui-react";
+import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const handleLogInClick = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("У вас нет метамаска!");
+    }
+
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Menu style={{ marginTop: "20px" }}>
       <Link href="/">
@@ -13,6 +32,19 @@ const Header = () => {
       <Link href="/show">
         <Menu.Item>Посмотреть контакт</Menu.Item>
       </Link>
+      <Menu.Item position="right">
+        {!currentAccount ? (
+          <Button primary onClick={handleLogInClick}>
+            Вход
+          </Button>
+        ) : (
+          <Link href="/user">
+            <Button primary onClick={handleLogInClick}>
+              {currentAccount}
+            </Button>
+          </Link>
+        )}
+      </Menu.Item>
     </Menu>
   );
 };
---
+++ client/pages/user.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const UserPage = () => {
+    return ( <Layout>User</Layout> );
+}
+ 
+export default UserPage;
